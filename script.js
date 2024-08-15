57e1affe684e74a0c4611732973c48a847d0ec94
Zaahiid <zahidrsheikh01@gmail.com>
2024-08-16 00:01:59 +0530

    Added copy to Clipboard button 📋

--- script.js
+++ script.js
@@ -235,3 +235,12 @@ function convertWithRules(word) {
     .map((char) => alphabets[char] || char)
     .join("");
 }
+
+function copyToClipboard() {
+  const outputText = document.getElementById("outputText").innerText;
+  navigator.clipboard.writeText(outputText).then(() => {
+    alert("Text copied to clipboard!");
+  }).catch(err => {
+    console.error("Failed to copy text: ", err);
+  });
+}
