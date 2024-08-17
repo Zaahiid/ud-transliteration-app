69b676f96bf00cc0d670fe8a1b55abc122136196
Zaahiid <zahidrsheikh01@gmail.com>
2024-08-17 23:46:58 +0530

    Added words and Style

--- script.js
+++ script.js
@@ -238,9 +238,12 @@ function convertWithRules(word) {
 
 function copyToClipboard() {
   const outputText = document.getElementById("outputText").innerText;
-  navigator.clipboard.writeText(outputText).then(() => {
-    alert("Text copied to clipboard!");
-  }).catch(err => {
-    console.error("Failed to copy text: ", err);
-  });
+  navigator.clipboard
+    .writeText(outputText)
+    .then(() => {
+      alert("Text copied to clipboard!");
+    })
+    .catch((err) => {
+      console.error("Failed to copy text: ", err);
+    });
 }
